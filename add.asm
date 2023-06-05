49476b9aed74921e1a16f3895bd0f6ff3cad0c23
RAHUL KUMAR <86794622+RAHUL-HACKER-HACKER@users.noreply.github.com>
2023-06-05 12:31:14 +0530

    commit

---
+++ add.asm
@@ -0,0 +1,18 @@
+mov a,#25h
+mov b,#12h
+add a,b
+mov 40h,a
+
+mov a,#25h
+subb a,b
+mov 41h,a
+
+mov a,#25
+mul ab
+mov 42h,a
+
+
+
+
+LCALL 0003H
+END
---
+++ count.cpp
@@ -0,0 +1,9 @@
+#include<reg51.h>
+void main(void)
+{
+	unsigned char z;
+	for(z=0;z<=255;z++)
+	{
+		p1=z;
+	}
+}
